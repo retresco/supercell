02832094e60f18dc3fc1e93e7b907ac2de42a478
Andreas Peldszus <andreas.peldszus@retresco.de>
2020-08-05 18:51:12 +0200

    Add Python 3.8 to PyPi classifiers

--- setup.py
+++ setup.py
@@ -69,6 +69,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.4',
